159e9d91f3117fef54a08033a41fe2c8a04f14cf
driazati <driazati@users.noreply.github.com>
2021-06-30 12:09:44 -0700

    Add test to check public APIs against master
    This implements/fixes #58617. If a PR adds a new bindings to the Python APIs, this will show up as a failing test, which they can fix by adding release notes to the PR body. The check errs on the side of caution in the face of flakiness, so if it can't reach S3 or doesn't have the env variables it expects it just quits.
    
    Also this fixes an issue in the `torch._C` api bindings test where it wasn't actually testing the list defined in the file.
    
    ghstack-source-id: 7fbb4b74762a17e1aff5d17e67647ca03081b99d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/60927

--- test/test_public_bindings.py
+++ test/test_public_bindings.py
@@ -1,10 +1,81 @@
-from torch.testing._internal.common_utils import run_tests
+from torch.testing._internal.common_utils import run_tests, IS_IN_CI
 
 import torch
 import unittest
+import urllib.request
+import io
+import zipfile
+import pathlib
+import sys
+import textwrap
+import subprocess
+import os
+
+
+from typing import Set
+
+
+PR_BODY = os.getenv("GITHUB_PR_BODY", None)
+
+
+def parse_api_list(api_list: str) -> Set[str]:
+    return set(api_list.strip().split("\n"))
+
+
+def fetch_master_python_apis() -> Set[str]:
+    APIS_URL = "https://gha-artifacts.s3.amazonaws.com/pytorch/pytorch/ci/apis.zip"
+
+    with urllib.request.urlopen(APIS_URL) as f:
+        zip_buf = io.BytesIO(f.read())
+
+    with zipfile.ZipFile(zip_buf) as f:
+        return parse_api_list(f.read("apis.log").decode())
+
+
+def fetch_current_python_apis() -> Set[str]:
+    repo_dir = pathlib.Path(__file__).resolve().parent.parent
+    list_apis_script = repo_dir / "scripts" / "list_apis.py"
+    cmd = [sys.executable, str(list_apis_script), "--module", "torch", "--public"]
+    proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if proc.returncode != 0:
+        raise RuntimeError(f"Failed to generate APIs from local build: {proc.stderr}")
+
+    return parse_api_list(proc.stdout.decode())
+
 
 class TestPublicBindings(unittest.TestCase):
-    def test_no_new_bindings(self):
+    @unittest.skipIf(not IS_IN_CI or PR_BODY is None, "API bindings are only tested on PRs")
+    def test_no_new_python_bindings(self):
+        """
+        This implements https://github.com/pytorch/pytorch/issues/58617, which
+        will fail if the PR this runs on introduces new public APIs. A public
+        API is considered anything that is not a child of a system module (e.g.
+        'sys', 'typing', etc.) that is a child of the 'torch' module (e.g.
+        'torch.ones', 'torch.jit.script', etc.). Unlike the C bindings there are
+        several thousand Python API bindings, so we rely on a list stored in S3
+        and updated by a job on master in .github/templates/linux_ci_workflow.yml.j2
+
+        If this test is run on master or can't reach S3, it is a no-op.
+        """
+        try:
+            master_public_bindings = fetch_master_python_apis()
+            current_public_bindings = fetch_current_python_apis()
+        except Exception as e:
+            print(f"failed to check public Python APIs: {e}")
+            return
+
+        difference = master_public_bindings.symmetric_difference(current_public_bindings)
+        msg = textwrap.dedent(f"""
+            New public API(s) were added: {', '.join(difference)}
+
+            To silence this error, add a release notes section to your PR body:
+
+                ### Release Notes
+                * the new feature""")
+        self.assertTrue(difference != set() and "### Release Notes" not in PR_BODY, msg)
+
+    def test_no_new_c_bindings(self):
         """
         This test aims to stop the introduction of new JIT bindings into torch._C
         whose names do not start with _. Such bindings are made available as
@@ -23,24 +94,27 @@ class TestPublicBindings(unittest.TestCase):
         #   {elem for elem in dir(torch._C) if not elem.startswith("_")}
         #
         torch_C_allowlist = {
-            "AVG",
             "AggregationType",
+            "AliasDb",
             "AnyType",
             "Argument",
             "ArgumentSpec",
-            "BFloat16StorageBase",
+            "autocast_decrement_nesting",
+            "autocast_increment_nesting",
+            "AVG",
             "BenchmarkConfig",
             "BenchmarkExecutionStats",
+            "BFloat16StorageBase",
             "Block",
             "BoolStorageBase",
             "BoolType",
             "BufferDict",
             "ByteStorageBase",
-            "CONV_BN_FUSION",
             "CallStack",
             "Capsule",
             "CharStorageBase",
             "ClassType",
+            "clear_autocast_cache",
             "Code",
             "CompilationUnit",
             "CompleteArgumentSpec",
@@ -49,6 +123,8 @@ class TestPublicBindings(unittest.TestCase):
             "ComplexType",
             "ConcreteModuleType",
             "ConcreteModuleTypeBuilder",
+            "CONV_BN_FUSION",
+            "cpp",
             "CudaBFloat16StorageBase",
             "CudaBFloat16TensorBase",
             "CudaBoolStorageBase",
@@ -74,88 +150,35 @@ class TestPublicBindings(unittest.TestCase):
             "CudaShortStorageBase",
             "CudaShortTensorBase",
             "DeepCopyMemoTable",
+            "default_generator",
+            "device",
             "DeviceObjType",
             "DictType",
             "DisableTorchFunction",
             "DoubleStorageBase",
+            "dtype",
             "EnumType",
             "ErrorReport",
             "ExecutionPlan",
-            "FUSE_ADD_RELU",
             "FatalError",
             "FileCheck",
+            "finfo",
             "FloatStorageBase",
             "FloatType",
+            "fork",
             "FunctionSchema",
+            "FUSE_ADD_RELU",
             "Future",
             "FutureType",
             "Generator",
-            "Gradient",
-            "Graph",
-            "GraphExecutorState",
-            "HOIST_CONV_PACKED_PARAMS",
-            "HalfStorageBase",
-            "INSERT_FOLD_PREPACK_OPS",
-            "IODescriptor",
-            "InferredType",
-            "IntStorageBase",
-            "IntType",
-            "InterfaceType",
-            "JITException",
-            "ListType",
-            "LiteScriptModule",
-            "LockingLogger",
-            "LoggerBase",
-            "LongStorageBase",
-            "MobileOptimizerType",
-            "ModuleDict",
-            "Node",
-            "NoneType",
-            "NoopLogger",
-            "NumberType",
-            "OptionalType",
-            "ParameterDict",
-            "PyObjectType",
-            "PyTorchFileReader",
-            "PyTorchFileWriter",
-            "QInt32StorageBase",
-            "QInt8StorageBase",
-            "QUInt4x2StorageBase",
-            "QUInt8StorageBase",
-            "REMOVE_DROPOUT",
-            "RRefType",
-            "SUM",
-            "ScriptClass",
-            "ScriptClassFunction",
-            "ScriptFunction",
-            "ScriptMethod",
-            "ScriptModule",
-            "ScriptObject",
-            "ShortStorageBase",
-            "Size",
-            "StaticRuntime",
-            "Stream",
-            "StreamObjType",
-            "StringType",
-            "TensorType",
-            "ThroughputBenchmark",
-            "TracingState",
-            "TupleType",
-            "Type",
-            "Use",
-            "Value",
-            "autocast_decrement_nesting",
-            "autocast_increment_nesting",
-            "clear_autocast_cache",
-            "cpp",
-            "default_generator",
-            "device",
-            "dtype",
-            "finfo",
-            "fork",
+            "get_autocast_cpu_dtype",
             "get_default_dtype",
             "get_num_interop_threads",
             "get_num_threads",
+            "Gradient",
+            "Graph",
+            "GraphExecutorState",
+            "HalfStorageBase",
             "has_cuda",
             "has_cudnn",
             "has_lapack",
@@ -163,32 +186,95 @@ class TestPublicBindings(unittest.TestCase):
             "has_mkldnn",
             "has_mlc",
             "has_openmp",
+            "HOIST_CONV_PACKED_PARAMS",
             "iinfo",
-            "import_ir_module",
             "import_ir_module_from_buffer",
+            "import_ir_module",
+            "InferredType",
             "init_num_threads",
+            "INSERT_FOLD_PREPACK_OPS",
+            "InterfaceType",
+            "IntStorageBase",
+            "IntType",
+            "IODescriptor",
             "is_anomaly_enabled",
+            "is_autocast_cpu_enabled",
             "is_autocast_enabled",
             "is_grad_enabled",
+            "is_inference_mode_enabled",
+            "JITException",
             "layout",
+            "ListType",
+            "LiteScriptModule",
+            "LockingLogger",
+            "LoggerBase",
+            "LongStorageBase",
             "memory_format",
             "merge_type_from_type_comment",
+            "MobileOptimizerType",
+            "ModuleDict",
+            "Node",
+            "NoneType",
+            "NoopLogger",
+            "NumberType",
+            "OperatorInfo",
+            "OptionalType",
+            "ParameterDict",
             "parse_ir",
             "parse_schema",
             "parse_type_comment",
+            "PyObjectType",
+            "PyTorchFileReader",
+            "PyTorchFileWriter",
+            "QInt32StorageBase",
+            "QInt8StorageBase",
             "qscheme",
+            "QUInt4x2StorageBase",
+            "QUInt8StorageBase",
+            "read_vitals",
+            "REMOVE_DROPOUT",
+            "RRefType",
+            "ScriptClass",
+            "ScriptClassFunction",
+            "ScriptDict",
+            "ScriptDictIterator",
+            "ScriptDictKeyIterator",
+            "ScriptFunction",
+            "ScriptMethod",
+            "ScriptModule",
+            "ScriptModuleSerializer",
+            "ScriptObject",
             "set_anomaly_enabled",
+            "set_autocast_cpu_dtype",
+            "set_autocast_cpu_enabled",
             "set_autocast_enabled",
             "set_flush_denormal",
             "set_num_interop_threads",
             "set_num_threads",
+            "set_vital",
+            "ShortStorageBase",
+            "Size",
+            "StaticModule",
+            "StorageContext",
+            "Stream",
+            "StreamObjType",
+            "StringType",
+            "SUM",
+            "TensorType",
+            "ThroughputBenchmark",
+            "TracingState",
+            "TupleType",
+            "Type",
             "unify_type_list",
+            "Use",
+            "Value",
+            "vitals_enabled",
             "wait",
         }
         torch_C_bindings = {elem for elem in dir(torch._C) if not elem.startswith("_")}
 
         # Check that both sets above have the same elements as each other.
-        self.assertSetEqual(torch_C_bindings.symmetric_difference(torch_C_bindings), set())
+        self.assertSetEqual(torch_C_allowlist.symmetric_difference(torch_C_bindings), set())
 
 
 if __name__ == '__main__':
